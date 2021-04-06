c5b83ff609e641662d69e6687fd9f41111db2c99
parazitexe <chobak789>
2021-04-07 00:50:10 +0300

    Event Bindings https://www.udemy.com/course/angular-complete-guide/learn/lecture/15737402#overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'udemyAngular';
   name = 'Filin';
+  img:string = "";
+  img1 = "https://via.placeholder.com/480x360?text=Vasia%20Filin";
+  img2 = "https://via.placeholder.com/400x300?text=Pefia%20Suslik";
+
+  ngOnInit(): void {
+    this.img = this.img1
+    setTimeout(()=>{
+      this.img = this.img2
+    },2000)
+  }
+
+
+
 }
