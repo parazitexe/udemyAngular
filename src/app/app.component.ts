ec32e21d68441388ccc66a83d83066a78879c4d6
parazitexe <chobak789>
2021-04-11 14:26:28 +0300

    internal commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,11 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'udemyAngular';
   name = 'Filin';
+  inputValue = "";
   img:string = "";
   img1 = "https://via.placeholder.com/480x360?text=Vasia%20Filin";
-  img2 = "https://via.placeholder.com/400x300?text=Pefia%20Suslik";
+  img2 = "https://via.placeholder.com/400x300?text=Petia%20Suslik";
+
 
   ngOnInit(): void {
     this.img = this.img1
@@ -20,5 +22,16 @@ export class AppComponent implements OnInit{
   }
 
 
+  onChanged(event:Event){
+
+    this.inputValue = (<HTMLInputElement>(<KeyboardEvent>event).target).value;
+  }
+  onBlur(value:string){
+    this.inputValue = value;
+  }
+
+  onClick(){
+    console.log("onClick")
+  }
 
 }
